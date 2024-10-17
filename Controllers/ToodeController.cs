f8663b4ead7012096ad269f33984b4d1b9e3e3d4
opilane <opilane@HP-CZC2349HTQ>
2024-10-17 11:23:19 +0300

    Lõpetatud API päringud koos mudeli ja nimekirjaga

---
+++ Controllers/ToodeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VeebYekasov1.Models;
+
+namespace VeebYekasov1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToodeController : ControllerBase
+    {
+        private static Toode _toode = new Toode(1, "Koola", 1.5, true);
+
+        // GET: toode
+        [HttpGet]
+        public Toode GetToode()
+        {
+            return _toode;
+        }
+
+        // GET: toode/suurenda-hinda
+        [HttpGet("suurenda-hinda")]
+        public Toode SuurendaHinda()
+        {
+            _toode.Price = _toode.Price + 1;
+            return _toode;
+        }
+         //Switch to opposite isAvtive
+        [HttpGet("toggle-activity")]
+        public Toode ToggleActivity()
+        {
+            _toode.IsActive = !_toode.IsActive;
+            return _toode;
+        }
+
+        //Change name 
+        [HttpPut("change-name/{newName}")]
+        public Toode ChangeName(string newName)
+        {
+            _toode.Name = newName;
+            return _toode;
+        }
+
+        //Multiply price by
+        [HttpPut("multiply-price/{factor}")]
+        public Toode MultiplyPrice(double factor)
+        {
+            _toode.Price *= factor;
+            return _toode;
+        }
+    }
+}
---
+++ Controllers/TootedController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using VeebYekasov1.Models;
+
+namespace VeebYekasov1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TootedController : ControllerBase
+    {
+        private static List<Toode> _tooted = new List<Toode>{
+        new Toode(1,"Koola", 1.5, true),
+        new Toode(2,"Fanta", 1.0, false),
+        new Toode(3,"Sprite", 1.7, true),
+        new Toode(4,"Vichy", 2.0, true),
+        new Toode(5,"Vitamin well", 2.5, true)
+        };
+
+
+        // https://localhost:7052/tooted
+        [HttpGet]
+        public List<Toode> Get()
+        {
+            return _tooted;
+        }
+
+        [HttpGet("kustuta/{index}")]
+        public List<Toode> Delete(int index)
+        {
+            _tooted.RemoveAt(index);
+            return _tooted;
+        }
+
+        [HttpGet("kustuta2/{index}")]
+        public string Delete2(int index)
+        {
+            _tooted.RemoveAt(index);
+            return "Kustutatud!";
+        }
+
+        [HttpGet("lisa/{id}/{nimi}/{hind}/{aktiivne}")]
+        public List<Toode> Add(int id, string nimi, double hind, bool aktiivne)
+        {
+            Toode toode = new Toode(id, nimi, hind, aktiivne);
+            _tooted.Add(toode);
+            return _tooted;
+        }
+
+        [HttpGet("lisa")] // GET /tooted/lisa?id=1&nimi=Koola&hind=1.5&aktiivne=true
+        public List<Toode> Add2([FromQuery] int id, [FromQuery] string nimi, [FromQuery] double hind, [FromQuery] bool aktiivne)
+        {
+            Toode toode = new Toode(id, nimi, hind, aktiivne);
+            _tooted.Add(toode);
+            return _tooted;
+        }
+
+        [HttpGet("hind-dollaritesse/{kurss}")] // GET /tooted/hind-dollaritesse/1.5
+        public List<Toode> Dollaritesse(double kurss)
+        {
+            for (int i = 0; i < _tooted.Count; i++)
+            {
+                _tooted[i].Price = _tooted[i].Price * kurss;
+            }
+            return _tooted;
+        }
+
+        // või foreachina:
+
+        [HttpGet("hind-dollaritesse2/{kurss}")] // GET /tooted/hind-dollaritesse2/1.5
+        public List<Toode> Dollaritesse2(double kurss)
+        {
+            foreach (var t in _tooted)
+            {
+                t.Price = t.Price * kurss;
+            }
+
+            return _tooted;
+        }
+
+        //Delete all instantly
+        [HttpGet("kustuta3")]
+        public string Delete3()
+        {
+            _tooted.Clear();
+            return "Kustutatud!";
+        }
+
+        //Change all IsActive to false
+        [HttpGet("tofalse")]
+        public List<Toode> Tofalse()
+        {
+            foreach (var toode in _tooted)
+            {
+                toode.IsActive = false;
+            }
+            return _tooted;
+        }
+
+        //Return by the number
+        [HttpGet("byNumber")]
+        public Toode ByNumber(int index)
+        {
+            foreach (var toode in _tooted)
+            {
+                if (toode.Id == --index)
+                {
+                    return toode;
+                }
+            }
+            return null;
+        }
+
+        //Return highest price
+        [HttpGet("price")]
+        public Toode Price()
+        {
+            var maxPrice = _tooted.Max(p => p.Price);
+            int index = _tooted.FindIndex(p => p.Price == maxPrice);
+            return _tooted[index];
+        }
+    }
+}
